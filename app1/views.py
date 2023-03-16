c2c7f3f6766dad582f3870e50d5da14ad56bffd0
mshilpa54321 <mshilpa54321@gmail.com>
2023-03-16 19:13:09 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def preethi(request):
+    return render(request,'preethi.html')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def shilpa(request):
+    return render(request,'shilpa.html')
